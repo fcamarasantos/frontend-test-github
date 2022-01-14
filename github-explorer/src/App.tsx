540ffd6529453d825e2ac7c4bc1fa53550b58e76
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-01-14 20:13:54 -0300

    adicionado estilo global

--- github-explorer/src/App.tsx
+++ github-explorer/src/App.tsx
@@ -1,8 +1,10 @@
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
---
+++ github-explorer/src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #f0f2f5;
+    -webkit-font-smoothing: antialised;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
