b7e61bda9c9291f37db93d8a08853ab6882db53b
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-01-14 20:18:59 -0300

    adicionado font do Google Fonts

--- github-explorer/src/styles/global.ts
+++ github-explorer/src/styles/global.ts
@@ -7,11 +7,6 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  body {
-    background: #f0f2f5;
-    -webkit-font-smoothing: antialised;
-  }
-
   html {
     @media (max-width: 1080px) {
       font-size: 93.75%;
@@ -20,6 +15,20 @@ export const GlobalStyle = createGlobalStyle`
     @media (max-width: 720px) {
       font-size: 87.5%;
     }
+  }
+
+  body {
+    background: #f0f2f5;
+    -webkit-font-smoothing: antialised;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
