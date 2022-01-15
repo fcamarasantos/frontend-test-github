5c9022ac9c95c9c8b75465337c21ec3b1e045037
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-01-14 21:12:09 -0300

    header criado

--- github-explorer/src/App.tsx
+++ github-explorer/src/App.tsx
@@ -1,9 +1,10 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
       <GlobalStyle />
     </div>
   );
---
+++ github-explorer/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Content } from './styles'
+
+export function Header(){
+  return (
+    <Container>
+      <Content>
+        <h1>Github Explorer</h1>
+      </Content>
+    </Container>
+  )
+}
