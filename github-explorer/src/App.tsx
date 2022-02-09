596beb86c2be05088412d1b74bf9fcbe342993c3
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-02-09 11:50:22 -0300

    configuracao de navegacao entre rotas

--- github-explorer/src/App.tsx
+++ github-explorer/src/App.tsx
@@ -1,11 +1,15 @@
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import {MainRoutes} from './routes';
 
 export function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
+      <MainRoutes />
       <GlobalStyle />
-    </div>
+    </Router>
   );
 }
--- github-explorer/src/components/Sidebar/index.tsx
+++ github-explorer/src/components/Sidebar/index.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import { Content } from './styles'
 
 export function Sidebar(){
   return (
     <nav>
       <Content>
-        <a href="/">Commits</a>
-        <a href="/">Pull Requests</a>
-        <a href="/">Issues</a>
+        <Link to="/commits">Commits</Link>
+        <Link to="/pullrequests">Pull Requests</Link>
+        <Link to="/issues">Issues</Link>
       </Content>
     </nav>
   )
