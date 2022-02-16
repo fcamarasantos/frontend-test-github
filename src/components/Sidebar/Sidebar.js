f2ac66023c610c05b00cdc3bb2c82beb65d5dd53
Gustavo Inacio <gutiinacio@gmai.com>
2022-02-16 17:13:24 -0300

    searchingrepos working with pagination and listing commits with pagination

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -9,7 +9,7 @@ function Sidebar({triggerID, ...props}) {
         M.AutoInit();
     });
     
-
+    const dashboardBaseUrl = `/Dashboard`;
   return (
     <div className='sidebar-container'>
         <ul id={`${triggerID}`} className="sidenav">
@@ -19,28 +19,38 @@ function Sidebar({triggerID, ...props}) {
                     <div className="background">
                         <img src="https://picsum.photos/200" />
                     </div>
-                <a href="#user"><img className="circle" src="images/yuna.jpg" /></a>
-                <a href="#name"><span className="white-text name">John Doe</span></a>
-                <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
-            </div>
+                    <a href="#"><img className="circle" src="images/yuna.jpg" /></a>
+                    <a href="#"><span className="white-text name">John Doe</span></a>
+                    <a href="#"><span className="white-text email">jdandturk@gmail.com</span></a>
+                </div>
             </li>
             <li>
-                <Link className="waves-effect" to='/search'>
+                <Link className="waves-effect" to={`${dashboardBaseUrl}/search`}>
                     <i className="material-icons">search</i>
                     Pesquisar repositórios
                 </Link>
-                <Link className="waves-effect" to='/commits'>
+                {/* <Link className="waves-effect" to={`${dashboardBaseUrl}/commits`}>
                     <i className="material-icons">format_list_bulleted</i>
                     Commits 
                 </Link>
-                <Link className="waves-effect" to='/issues'>
+                <Link className="waves-effect" to={`${dashboardBaseUrl}/issues`}>
                     <i className="material-icons">error</i>
                     Issues
                 </Link>
-                <Link className="waves-effect" to='/pullRequests'>
+                <Link className="waves-effect" to={`${dashboardBaseUrl}/pullRequests`}>
                     <i className="material-icons">move_to_inbox</i>
                     Pull requests
                 </Link>
+                <a className="waves-effect">
+                    <i className="material-icons">move_to_inbox</i>
+                    Pull requests
+                </a> */}
+                {props.tabs.map((item) => (
+                    <a className="waves-effect" key={item.title} onClick={() => {props.navItemClicked(item)}}>
+                        <i className="material-icons">{item.icon}</i>
+                        {item.title}
+                    </a>
+                ))}
             </li>
         </ul>
     </div>
---
+++ src/pages/Dashboard/Home/Home.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react'
+import { getCommitList } from '../../../requests/getRepoList/getRepoList';
+import './Home.scss'
+
+function Home(props) {
+
+    const [commitsData, setCommitsData] = useState(null); 
+    const [actualPage, setActualPage] = useState(1);
+    const [lastPageNum, setLastPageNum] = useState(null);
+
+    useEffect(async () => {
+        console.log(props)
+        let commitsResp = await getCommitList(props.data.owner.login, props.data.name, {per_page: 50, page: actualPage}); 
+        setCommitsData(commitsResp);
+        setLastPageNum(commitsResp.lastPage);
+        console.log('commitsDtata', commitsResp)
+    },[]);
+
+    useEffect(async () => {
+        console.log(actualPage)
+        let commitsResp = await getCommitList(props.data.owner.login, props.data.name, {per_page: 50, page: actualPage}); 
+        setCommitsData(commitsResp);
+        setLastPageNum(commitsResp.lastPage);
+    }, [actualPage]);
+
+    const verifyPageNum = (pageNum) => {
+        return (pageNum >= 1 && pageNum <= lastPageNum);
+    }
+
+    const onPaginationChange = (pageNum) => {
+        verifyPageNum(pageNum)
+            setActualPage(pageNum)
+    }
+
+    const onBeforePagition = () => {
+        setActualPage(prev => {
+            if(verifyPageNum(prev -1)) return prev -1;
+            else return prev;
+        });
+    }
+    const onNextPagition = () => {
+        setActualPage(prev => {
+            if(verifyPageNum(+prev +1)) return +prev +1;
+            else return +prev;
+        });
+    }
+
+    let paginationIndexes = [];
+    for(let i =0; i < lastPageNum; i++){
+        let itemPageNum = i + 1;
+        paginationIndexes.push(
+            <li key={i} className={`waves-effect ${itemPageNum==actualPage ? 'active' : ''}`} onClick={() => {onPaginationChange(itemPageNum)}}><a className='like-a' href='#'>{itemPageNum}</a></li>
+        )
+    }
+
+  return (
+    <div className='HomeContainer container'>
+        <p className='table-title'>Tabela de commits</p>
+        <table>
+        <thead>
+          <tr>
+              <th>Autor</th>
+              <th>Descrição</th>
+              <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+            {commitsData && commitsData.map((item) => { 
+                let date = new Date(item.commit.author.date);
+                return(
+                    <tr key={item.sha}>
+                        <td>{item.commit.author.name}</td>
+                        <td>{item.commit.message}</td>
+                        <td>{`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`}</td>
+                    </tr>
+            )})}
+         
+        </tbody>
+      </table>
+      <div className='container'>
+          
+        <div className='row'>
+            <div className='col'>
+                <ul className="pagination">
+                    <li className={`${actualPage <= 1 ? 'disabled' : ''}`}><a href="#" onClick={onBeforePagition}><i className="material-icons">chevron_left</i></a></li>
+                    {paginationIndexes}
+                    <li className={`waves-effect ${actualPage >= lastPageNum ? 'disabled' : ''}`} ><a href="#" onClick={onNextPagition}><i className="material-icons">chevron_right</i></a></li>
+                </ul>
+            </div>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Home
